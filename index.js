e1cf670276f2df917d7c8fb4c4985cf9cd58ec7e
YuvMonday <yuvmonday@gmail.com>
2024-09-06 11:22:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,9 +13,7 @@ const WHATSAPP_PHONE_NUMBER_ID = '354266431113065';
 const ENDPOINT = `https://graph.facebook.com/v20.0/${WHATSAPP_PHONE_NUMBER_ID}/messages`;
 const TEMP_ACCESS_TOKEN = 'EAAWEh2d9r1IBOZBScO8NHlQpdUjeSlzmX9rYuEYWG7GjnKJyeW5ism0Cl4Ix8NSNGVPvNxxe4I5dLYLxTdKq9et5hd17xZAhi0vWYVtxnkQguJJY91kLNdQFvbJeqx7kxXD5jZAf9Aki8ZANwVxAr3HpRhFGg1QMB4jOwUlMqMF2c8ZCb0WZByxZAqldeIYYRzYoOJqd5ecq5sZBklVH7BeF4odro0cZD';
 const ACCESS_TOKEN = 'EAAWEh2d9r1IBO797zYZCdE4B7Je7WY70TCkhe18i0O8LFGDF7hLSps1ysRbEZCb6doXAf5MJg8CfhSdNqaaDaHVORAMPuyZC2ZBzOfR42nYOFC99nZB6C0wmw3qOUiZB5fdC4x1ph35JhkeW80YUBXfn0FCzAlZBdzf9Ro3jjaaM7pzuwAA1NXGRvJDckpI3tPjS3ciyZCHds0f7jvNgaRYOCZBJz8JB9DYpVnaUMh78ZD';
-const MESSAGE_TEXT = `This is sample message text`;
-// const MOBILE_NUMBERS = ['919179430062', '917000890062'];   //,'919582321892'];
-
+const MESSAGE_TEXT = `This is sample message text`
 
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
@@ -25,13 +23,14 @@ app.get("/getMessage", (req,res)=>{
     res.send({message:'Sucess'});
 })
 
-app.post("/api/postMessage", async (req,res)=>{
+app.post("/postMessage", async (req,res)=>{
     console.log(req.body);
-    let message = req?.body?.message;
+    let message = req?.body?.message===undefined?MESSAGE_TEXT:req?.body?.message;
     let phoneNumberList = req?.body?.phoneNumberList;
     let messageType = req?.body?.messageType;
 
-    if(messageType!=='text' && messageType!=='template') res.send({status:404,data:'For now allowed message type can only be text or template'})
+    if (phoneNumberList===undefined) res.send({status:400,data:'Please send a valid phone number list'})
+    else if(messageType!=='text' && messageType!=='template') res.send({status:404,data:'For now allowed message type can only be text or template'})
     else if(phoneNumberList===undefined || phoneNumberList.length<1) res.send({status:200,statusList:[]});
     else{
         let response = await sendMessage(message,messageType,phoneNumberList);    
