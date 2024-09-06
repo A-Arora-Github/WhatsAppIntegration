f2b4b40527751bbd0949a7c1653ddecb9de202a1
YuvMonday <yuvmonday@gmail.com>
2024-09-06 10:05:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const  cors = require("cors");
 
 const app = express();
 
-
-app.use(cors());
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
