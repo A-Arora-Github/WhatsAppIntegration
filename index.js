986196cbde5477948a8db6ad16ae2a6042a5cd95
YuvMonday <yuvmonday@gmail.com>
2024-09-06 10:31:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const app = express();
 
 app.use(cors());
 
+
+const WHATSAPP_PHONE_NUMBER_ID = '354266431113065';
+const ENDPOINT = `https://graph.facebook.com/v20.0/${WHATSAPP_PHONE_NUMBER_ID}/messages`;
+const TEMP_ACCESS_TOKEN = 'EAAWEh2d9r1IBOZBScO8NHlQpdUjeSlzmX9rYuEYWG7GjnKJyeW5ism0Cl4Ix8NSNGVPvNxxe4I5dLYLxTdKq9et5hd17xZAhi0vWYVtxnkQguJJY91kLNdQFvbJeqx7kxXD5jZAf9Aki8ZANwVxAr3HpRhFGg1QMB4jOwUlMqMF2c8ZCb0WZByxZAqldeIYYRzYoOJqd5ecq5sZBklVH7BeF4odro0cZD';
+const ACCESS_TOKEN = 'EAAWEh2d9r1IBO797zYZCdE4B7Je7WY70TCkhe18i0O8LFGDF7hLSps1ysRbEZCb6doXAf5MJg8CfhSdNqaaDaHVORAMPuyZC2ZBzOfR42nYOFC99nZB6C0wmw3qOUiZB5fdC4x1ph35JhkeW80YUBXfn0FCzAlZBdzf9Ro3jjaaM7pzuwAA1NXGRvJDckpI3tPjS3ciyZCHds0f7jvNgaRYOCZBJz8JB9DYpVnaUMh78ZD';
+const MESSAGE_TEXT = `This is sample message text`;
+const MOBILE_NUMBERS = ['919179430062', '917000890062'];   //,'919582321892'];
+
+
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
@@ -14,9 +23,76 @@ app.get("/getMessage", (req,res)=>{
     res.send({message:'Sucess'});
 })
 
-app.post("/postMessage", (req,res)=>{
-    res.send({message:'post message success'});
-})
+app.post("/postMessage", async (req,res)=>{
+    console.log(req.body);
+    let response = await sendMessage(req?.body?.message);
+
+    console.log(response);
+
+    if(response==='error') res.send({status:'error'});
+    else res.send({statusList:response});
+});
+
+
+let sendMessage = async (message)=>{
+    let response=[];
+
+    console.log('in send message');
+
+    await Promise.all(MOBILE_NUMBERS.map(async(number)=>{
+        let response2 = await sendMessageUtil(number,message);
+        response.push(response2);
+    }))
+    .then(res=>{
+        // console.log('res in promise all '+res);
+    });
+    
+
+    return response;
+}
+    
+
+let sendMessageUtil = async (to,message)=>{
+    // console.log('in send message');
+    let status;
+
+    let headers = {
+        'Content-Type':'application/json',
+        'Authorization':`Bearer ${ACCESS_TOKEN}`
+    };
+    let data = {
+        "messaging_product": "whatsapp",
+        "recipient_type": "individual",
+        "to": to,
+        // "type": "text",
+        // "text": {
+        // "preview_url": true,
+        // "body": MESSAGE_TEXT
+        // }
+        "type": "template",
+        "template": { 
+          "name": "hello_world",
+          "language": { 
+            "code": "en_US" 
+          } 
+        } 
+    };
+    await axios.post(ENDPOINT, data, {
+        headers: headers
+    })
+    .then((response) => {
+        status = response.status;
+        // console.log(response.status);
+    })
+    .catch((error) => {
+        status = error.status
+        console.log('in errrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrror');
+    });
+
+    return status;
+}
+
+
 
 app.listen(5000, () => {
   console.log("Running on port 5000.");
